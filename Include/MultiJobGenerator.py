4d1958be009195fbc45a3713262ffebcb7b78f84
Kyoengpil Lee <kplee@cern.ch>
2018-10-16 20:34:49 +0200

    debug

--- Include/MultiJobGenerator.py
+++ Include/MultiJobGenerator.py
@@ -69,7 +69,7 @@ class MultiJobGenerator:
         f_script.write("#!/bin/bash\n")
 
         for sampleType in self.dic_nJob.keys():
-            cmd = "source %s/hadd_all.sh" % sampleType
+            cmd = "source %s/%s/hadd_all.sh" % (self.WSPath, sampleType)
             f_script.write(cmd+"\n")
 
         f_script.close()
