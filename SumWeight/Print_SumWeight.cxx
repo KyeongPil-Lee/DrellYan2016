355ba072a105fdeb82041106f008e40efa4998ca
Kyoengpil Lee <kplee@cern.ch>
2018-10-16 20:49:35 +0200

    update to use easily

--- SumWeight/Print_SumWeight.cxx
+++ SumWeight/Print_SumWeight.cxx
@@ -1,7 +1,8 @@
 #include <Include/SimplePlotTools.h>
 
-void Print_SumWeight(TString rootFileName)
+void Print_SumWeight()
 {
+  TString rootFileName = _file0->GetName();
   TH1D* h_sumWeight = PlotTool::Get_Hist(rootFileName, "h_sumWeight");
   Double_t sumWeight = h_sumWeight->GetBinContent(1);
   printf("[SumWeight] %lf\n", sumWeight);
