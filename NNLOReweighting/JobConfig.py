b26a22856700fb8050c656835b0263e028c6a253
Kyoengpil Lee <kplee@cern.ch>
2018-10-16 20:55:58 +0200

    include all samples

--- NNLOReweighting/JobConfig.py
+++ NNLOReweighting/JobConfig.py
@@ -2,20 +2,29 @@ from Include.MultiJobGenerator import *
 
 generator = MultiJobGenerator()
 
-generator.jobName = "DYReweighting_TEST" # -- used for workspace name
+generator.jobName = "DYReweightHist" # -- used for workspace name
 generator.classCodePath = "NNLOReweighting/MakeHist_aMCNLO.h"
 generator.className = "HistProducer"
 
-generator.luminosity = 35863.818
+# generator.luminosity = 35863.818
+generator.luminosity = 1.0 # -- luminosity is not needed for x-section calculation
 generator.jsonName = "SampleInfo_v2p3.json" # -- under DY_ANALYZER_PATH/Include
 
 generator.dic_nJob = { 
-  "DYMuMu_M50to100": 3,
-  # "DYMuMu_M100to200": 1,
-  # "DYMuMu_M200to400": 1  
+  "DYMuMu_M15to50":     9,
+  "DYMuMu_M50to100":    9,
+  "DYMuMu_M100to200":   2,
+  "DYMuMu_M200to400":   1,
+  "DYMuMu_M400to500":   1,
+  "DYMuMu_M500to700":   1,
+  "DYMuMu_M700to800":   1,
+  "DYMuMu_M800to1000":  1,
+  "DYMuMu_M1000to1500": 1,
+  "DYMuMu_M1500to2000": 1,
+  "DYMuMu_M2000to3000": 1,
   }
 
-generator.outputPath = "/Users/KyeongPil_Lee/temp"
+generator.outputPath = "/scratch/kplee/DYJob"
 
 generator.Generate()
 
