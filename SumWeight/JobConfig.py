3f095f8ecfd1dd06bc663b28fef2dae512ad9bd4
Kyoengpil Lee <kplee@cern.ch>
2018-12-07 22:44:42 +0100

    check sum(weight) of background samples

--- SumWeight/JobConfig.py
+++ SumWeight/JobConfig.py
@@ -16,17 +16,13 @@ generator.jsonName = "SampleInfo_v2p3.json" # -- under DY_ANALYZER_PATH/Include
 #   }
 
 generator.dic_nJob = { 
-  "DYMuMu_M15to50":     9,
-  "DYMuMu_M50to100":    9,
-  "DYMuMu_M100to200":   2,
-  "DYMuMu_M200to400":   1,
-  "DYMuMu_M400to500":   1,
-  "DYMuMu_M500to700":   1,
-  "DYMuMu_M700to800":   1,
-  "DYMuMu_M800to1000":  1,
-  "DYMuMu_M1000to1500": 1,
-  "DYMuMu_M1500to2000": 1,
-  "DYMuMu_M2000to3000": 1,
+  "WJets": 10,
+  "ttbar": 10,
+  "tW":    1,
+  "tbarW": 1,
+  "WW":    1,
+  "WZ":    1,
+  "ZZ":    1,
   }
 
 generator.outputPath = "/scratch/kplee/DYJob"
