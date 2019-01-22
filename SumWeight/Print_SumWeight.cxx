2ac96d820adc0d5656a41b93a33e4d936881cba0
Kyoengpil Lee <kplee@cern.ch>
2019-01-22 20:21:14 +0900

    update print type

--- SumWeight/Print_SumWeight.cxx
+++ SumWeight/Print_SumWeight.cxx
@@ -5,5 +5,7 @@ void Print_SumWeight()
   TString rootFileName = _file0->GetName();
   TH1D* h_sumWeight = PlotTool::Get_Hist(rootFileName, "h_sumWeight");
   Double_t sumWeight = h_sumWeight->GetBinContent(1);
-  printf("[SumWeight] %lf\n", sumWeight);
+  printf("+++++++++++++++++++++++++++++++++++++++++++++++++\n");
+  printf("[ROOTFile: %s] -> SumWeight = %.1lf\n", rootFileName.Data(), sumWeight);
+  printf("+++++++++++++++++++++++++++++++++++++++++++++++++\n\n");
 }
