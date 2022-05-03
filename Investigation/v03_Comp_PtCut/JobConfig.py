ce43f224d6645d6c71aebba7fc3534864346801d
Kyeongpil Lee <kplee@Kyeongpils-MacBook-Pro.local>
2022-05-03 11:44:06 +0200

    switch to condor

--- Investigation/v03_Comp_PtCut/JobConfig.py
+++ Investigation/v03_Comp_PtCut/JobConfig.py
@@ -1,24 +1,29 @@
-from Include.MultiJobGenerator import *
+import os
 
-generator = MultiJobGenerator()
+def GetCWD():
+  cwd_fullPath = os.getcwd()
+  analyzerPath = os.environ["DY_ANALYZER_PATH"]
+  cwd = cwd_fullPath.split(analyzerPath+"/")[1] # -- start from the analyzer base path
+
+  return cwd
+
+
+from Include.CondorJobGenerator import *
+
+generator = MultiCondorJobGenerator()
 
 generator.jobName = "GenLevel" # -- used for workspace name
-generator.classCodePath = "Investigation/v03_Comp_PtCut/MakeHist_GenLevel.h"
+generator.ROOTCodePath = GetCWD()+"/MakeHist_GenLevel.h"
 generator.className = "HistProducer"
 
 generator.luminosity = 35863.818
 generator.jsonName = "SampleInfo_v2p3.json" # -- under DY_ANALYZER_PATH/Include
 
+# -- if "_skim" is added, it runs over skimmed ntuples
 generator.dic_nJob = { 
-  # "DYMuMu_M200to400":   2,
-  "DYMuMu_M50toInf":   10,
-  # "DYMuMu_M200to400_fixed":   2,
-  # "ZMuMuPowheg_M200to400":   2,
+    "DYMuMu_M50toInf":   30,
   }
 
-generator.outputPath = "/data9/Users/kplee/DrellYan2016Job"
-generator.bringOutput = True # -- bring output root file to the current working directory where JobConfig is executed
+generator.baseWSPath = "/data9/Users/kplee/DrellYan2016Job"
 
 generator.Generate()
-
-
